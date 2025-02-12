2a95b1dea80ca8cbe764a02398937c9af4120b83
Shefat Ahmed <shefata69@gmail.com>
2025-02-12 19:30:06 +0600

    setup counter feature in redux

--- src/redux/features/counterSlice.tsx
+++ src/redux/features/counterSlice.tsx
@@ -1,14 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {counter: 0}
+const initialState = { counter: 0 };
 
 const counterSlice = createSlice({
-        name: "counter",
-        initialState,
-        reducers: {
-        }
-})
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.counter = state.counter + 1;
+    },
+    decrement: (state) => {
+      state.counter = state.counter + 1;
+    },
+  },
+});
 
-export const {} = counterSlice.actions;
+export const { increment, decrement } = counterSlice.actions;
 
 export default counterSlice.reducer;
